b593bbcd4a9dfb41d585a2417bec21e452c50fee
kristoferandres <45108172+kristoferandres@users.noreply.github.com>
2022-03-03 02:58:50 +0200

    Add files via upload

---
+++ iseseisev_töö.py
@@ -0,0 +1,21 @@
+# 03.03.2022
+# iseseisev töö
+# Kristofer Andres
+import random
+loop = 0
+oige = 0
+while loop != 10:
+    loop += 1
+    earv = random.randint(0,10)
+    tarv = random.randint(0,10)
+    ovastus = earv * tarv
+    print(f"{loop}. {earv} * {tarv} = ?")
+    kvastus = int(input("? = "))
+    if ovastus == kvastus:
+        print("õige")
+        print("")
+        oige +=1
+    else:
+        print("väär")
+        print("")
+print(f"vastasid {oige}/10 õigesti")
